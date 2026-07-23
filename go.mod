8d65a41dfb5ff2169006a93ee40215044f400ad6
rcmadhankumar <madhankumar.chellamuthu@suse.com>
2026-07-23 09:27:21 +0530

    BUILD: SLE12: revert "btrfs: depend on kernel UAPI instead of libbtrfs"
    This is a partial revert of commit 52f82acb7b39 ("btrfs: depend on
    kernel UAPI instead of libbtrfs"), namely switching from
    github.com/containers/btrfs/v2 to v1.
    
    On SLE 12, our UAPI headers are too old, resulting in us being unable to
    build the btrfs driver with the new headers. This patch is only needed
    for SLE-12.
    
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/Microsoft/hcsshim v0.12.9
 	github.com/checkpoint-restore/checkpointctl v1.3.0
 	github.com/checkpoint-restore/go-criu/v7 v7.2.0
-	github.com/containerd/btrfs/v2 v2.0.0
+	github.com/containerd/btrfs v1.0.1
 	github.com/containerd/cgroups/v3 v3.0.3
 	github.com/containerd/console v1.0.4
 	github.com/containerd/containerd/api v1.8.0
--- vendor/github.com/containerd/btrfs/btrfs.c
+++ vendor/github.com/containerd/btrfs/btrfs.c
@@ -14,16 +14,21 @@
    limitations under the License.
 */
 
+#include <stddef.h>
 #include <string.h>
+#include <linux/magic.h>
+#include <btrfs/ioctl.h>
+#include <btrfs/ctree.h>
+
 #include "btrfs.h"
 
 void unpack_root_item(struct gosafe_btrfs_root_item* dst, struct btrfs_root_item* src) {
 	memcpy(dst->uuid, src->uuid, BTRFS_UUID_SIZE);
 	memcpy(dst->parent_uuid, src->parent_uuid, BTRFS_UUID_SIZE);
 	memcpy(dst->received_uuid, src->received_uuid, BTRFS_UUID_SIZE);
-	dst->generation = src->generation;
-	dst->otransid = src->otransid;
-	dst->flags = src->flags;
+	dst->gen = btrfs_root_generation(src);
+	dst->ogen = btrfs_root_otransid(src);
+	dst->flags = btrfs_root_flags(src);
 }
 
 /* unpack_root_ref(struct gosafe_btrfs_root_ref* dst, struct btrfs_root_ref* src) { */
